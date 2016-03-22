48beb8663dd0c93ff6e2d3c44c52ff92e376d617
Trent Casey <trcasey@gmail.com>
2016-03-21 20:42:49 -0400

    finished second version of the program. Includes more personalized formatting.

--- program.py
+++ program.py
@@ -8,16 +8,18 @@ print()
 the_number = random.randint(0, 100)
 guess = -1
 
+name = input('Player what is your name? ')
+
 while guess != the_number:
     guess_text = input('Guess a number between 0 and 100: ')
     guess = int(guess_text)
 
     if guess < the_number:
-        print('too low')
+        print('Sorry {1}, your guess of {0} was too LOW.'.format(guess, name))
     elif guess > the_number:
-        print('too high')
+        print('Sorry {1} your guess of {0} was too HIGH'.format(guess, name))
     else:
-        print('you win!')
+        print('Excellent work {}, you won! It was {}!'.format(name, guess))
 
 print('done!')
 

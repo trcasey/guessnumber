56e72f132d18ff8850c788fecad6cd5fdd42e330
Trent Casey <trcasey@gmail.com>
2016-03-21 20:35:33 -0400

    finished first version of the program.

--- program.py
+++ program.py
@@ -6,9 +6,19 @@ print('------------------------------------------')
 print()
 
 the_number = random.randint(0, 100)
+guess = -1
 
-guess_text = input('Guess a number between 0 and 100: ')
-guess = int(guess_text)
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
 
+    if guess < the_number:
+        print('too low')
+    elif guess > the_number:
+        print('too high')
+    else:
+        print('you win!')
+
+print('done!')
 
 
